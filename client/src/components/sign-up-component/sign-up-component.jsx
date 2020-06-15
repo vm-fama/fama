1d5cc677f0afb4084a9986c78f566c35b8d29ee0
mcolligan <mcolligan1@gmail.com>
2020-06-15 13:02:32 -0500

    Created a test form to use in implementing backend firebase. Form will be nuked (or refactored) when firebase implementation is complete

---
+++ client/src/components/sign-up-component/sign-up-component.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+
+export default class SignUp extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password1: '',
+      password2: '',
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    const { firstName, lastName, email, password1, password2 } = this.state;
+
+    const isInvalid =
+      password1 !== password2 || password1 === '' || email === '';
+
+    return (
+      <div>
+        <span>Tester form for sign up</span>
+        <form>
+          <div>
+            <input
+              name='firstName'
+              value={firstName}
+              onChange={this.handleChange}
+              placeholder='First Name'
+            ></input>
+            <input
+              name='lastName'
+              value={lastName}
+              onChange={this.handleChange}
+              placeholder='Last Name'
+            ></input>
+          </div>
+          <div>
+            <input
+              name='email'
+              value={email}
+              onChange={this.handleChange}
+              placeholder='Email'
+              type='email'
+            ></input>
+          </div>
+          <div>
+            <input
+              name='password1'
+              value={password1}
+              onChange={this.handleChange}
+              placeholder='Password'
+              type='password'
+            ></input>
+            <input
+              name='password2'
+              value={password2}
+              onChange={this.handleChange}
+              placeholder='confirm password'
+              type='password'
+            ></input>
+          </div>
+          <button disabled={isInvalid} type='submit' onClick={this.onSubmit}>
+            Create Account
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
---
+++ client/src/pages/sign-up-in/sign-in-up-page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import SignUp from '../../components/sign-up-component/sign-up-component';
+
+const SignInOrUp = () => (
+  <div>
+    <SignUp />
+  </div>
+);
+
+export default SignInOrUp;
