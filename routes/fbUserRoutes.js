ae87aed884e91d5f1b5430ada66e3d671a2f1fc6
mcolligan <mcolligan1@gmail.com>
2020-06-29 13:48:14 -0500

    init commit for sign-in beta

--- routes/fbUserRoutes.js
+++ routes/fbUserRoutes.js
@@ -4,16 +4,6 @@ const fb = require('../firebase/firebase');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-  /*
-    Req.body for a sign-up will look like this -
-    {
-	    "firstName": "Myles",
-	    "lastName": "Colligan",
-	    "email":"myles@fama.com",
-	    "password1": "1234567",
-	    "password2": "1234567"
-    }
-  */
   fb.getUser(req.body).then((userDoc) => {
     if (userDoc.exists) {
       res.status(403).send({ error: 'User with that email already exists' });
